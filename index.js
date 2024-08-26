7755b8a63c5fe3a822243e8cdffbbefd588a06ab
Oluwadamilola Adedeji <ebenoluwadamilola@gmail.com>
2024-08-26 16:15:40 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import getPosts, { getPostLength } from "./post-controller.js";
+console.log(getPosts());
+console.log(getPostLength());
+// const {generateRandomNumbers, celciusToFahrenheit} = require('./utilz')
+
+// console.log(`Random Number: ${generateRandomNumbers()}`)
+// console.log(`Celcius to fahrenheit: ${celciusToFahrenheit(0)}`)
